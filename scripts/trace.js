1592d56cb8cef53df50b9d2f322e2563e274d3e6
Eric <18709270892@163.com>
2021-08-25 00:50:50 +0800

    init

---
+++ scripts/trace.js
@@ -0,0 +1,22 @@
+$(function () {
+  var tickProcessIns = new TickProcess({
+    el: '#axis',
+    // loop: true,
+    data: [
+      '2020-09-02',
+      '2020-05-02',
+      '2020-05-03',
+      '2020-05-04',
+      '2020-05-05',
+      '2020-05-06',
+      '2020-05-02',
+      '2020-05-03',
+    ],
+    onChange: function (v) {
+      console.log(v)
+    },
+  })
+
+  tickProcessIns.init()
+  tickProcessIns.play()
+})
