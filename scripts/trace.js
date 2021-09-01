67a9ce89b4fc9de0d0e2bcc3b0c102d1b3b7f5a0
Eric <18709270892@163.com>
2021-09-01 20:54:02 +0800

    feat: initial data in init()

--- scripts/trace.js
+++ scripts/trace.js
@@ -1,31 +1,36 @@
+var tickProcessIns = new TickProcess({
+  el: '#axis',
+  // loop: true,
+  onChange: function (v) {
+    console.log(v)
+  },
+  onEnd: function () {
+    $('#play').text('>')
+  },
+})
 $(function () {
-  var tickProcessIns = new TickProcess({
-    el: '#axis',
-    // loop: true,
-    data: [
-      '2020-09-02',
-      '2020-05-02',
-      '2020-05-03',
-      '2020-05-04',
-      '2020-05-05',
-      '2020-05-06',
-      '2020-05-02',
-      '2020-05-03',
-    ],
-    onChange: function (v) {
-      console.log(v)
-    },
-  })
+  var data = [
+    '2020-09-02',
+    '2020-05-02',
+    '2020-05-03',
+    '2020-05-04',
+    '2020-05-05',
+    '2020-05-06',
+    '2020-05-02',
+    '2020-05-03',
+  ]
 
-  tickProcessIns.init()
-
+  tickProcessIns.init(data)
+})
+$(function () {
   $('#play').on('click', function (e) {
     console.log(tickProcessIns.playing)
     if (tickProcessIns.playing) {
-
       tickProcessIns.pause()
+      $('#play').text('>')
       return
     }
+    $('#play').text('||')
     tickProcessIns.play()
   })
 })
